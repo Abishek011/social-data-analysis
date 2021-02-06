ada792e28810376b3a23de7c69788c4614b26005
Abishek011 <rajaramsow@gmail.com>
2021-02-06 14:42:07 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+
+app.get('/:key/:count', async (req, res) => {
+
+    let runPy = new Promise(function (resolve, reject) {
+
+        const pychild = require('child_process');
+        const pyprog = pychild.spawn('python3', ['main.py']);
+        pyprog.stdout.on('data', function (data) {
+            console.log("ds", data);
+            resolve(data);
+        });
+
+        pyprog.stderr.on('data', (data) => {
+
+            reject(data);
+        });
+        pyprog.stdin.write(JSON.stringify([req.params.key, req.params.count]));
+        pyprog.stdin.end();
+    });
+
+        runPy.then(function (fromRunpy) {
+            console.log(fromRunpy.toString());
+            res.end(fromRunpy);
+        }).catch(err => {
+            console.log("error");
+        });
+})
+
+app.listen(4000, () => console.log('Application listening on port 4000!')) 
